c8327764930957a614077fc9285a2108979db071
Eugene Mironov <helper2424@gmail.com>
2019-02-18 13:27:46 +0500

    Add basic project structure

---
+++ README.md
@@ -0,0 +1,15 @@
+### The server side part of s30s project
+
+To start development server run 
+```
+npm run dev
+```
+
+To build the last version run
+```
+npm  run build
+```
+
+### Deploy
+
+Need to add deploy data
---
+++ src/index.ts
@@ -0,0 +1,14 @@
+// import Koa from 'koa'
+// import withWebSocket from 'koa-websocket'
+
+// const Server = withWebSocket(new Koa())
+// Server.ws
+//   .use(general.setUser)
+//   .use(rte.router.routes())
+
+// Server.listen(3100)
+
+
+let a = 'test'
+console.log(a + '23 ')
+console.log('HER')
